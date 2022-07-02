19144ade194ee2bb755bb9e59f58d9ce3314d963
klkranz <kkranz@protonmail.com>
2022-07-01 19:41:20 -0600

    First commit of Mystery Game - game play and first story

---
+++ Game.py
@@ -0,0 +1,11 @@
+# import modules
+from MurderAtChromaManor import *
+
+# Introduction
+print(f'Welcome!  You have been sent as the Detective Chief Inspector to investigate a crime.')
+DCI = input('What name would you like to go by Detective Chief Inspector?\n')
+story = "The Murder at Chroma Manor"  # Eventually this will be either a menu to pick the story or generated randomly.
+print(f'Welcome to {story}, Detective Chief Inspector {DCI}.')
+print(setting)
+print(f'What do you have for me today, {det_sergeant}?')
+print(intro)
---
+++ MurderAtChromaManor.py
@@ -0,0 +1,83 @@
+# import variables from Game.py
+from Game import *
+
+setting = "As your car pulls up the long drive, the imposing manor comes into view. It appears weathered and faded, " \
+          "but stoic and solid. Detective Sergeant Lewis appears to be waiting just outside the grand entrance for you."
+det_sergeant = "Detective Sergeant Lewis"
+intro = 'Scratching his head, Detective Sergeant Lewis says, “We have an unusual situation for you to investigate ' \
+        'today. A gunshot was heard and everyone came running to find Col. Chroma dead with a gun on the floor ' \
+        'behind him, and Capt. Lavender was seated in the next chair. One of the witnesses called it in saying they ' \
+        'had the suspect, and were going to lock the door to the scene of the crime. When Constable Bredon arrived, ' \
+        'he found signs that multiple attempts on the victim’s life were made with various methods. What’s more, ' \
+        'Medical Examiner Stevenson says the shot is unlikely to have killed him. The question seems to be who ' \
+        'didn’t try to kill him. The witnesses are all in the conservatory and we took the housekeeper’s keys and ' \
+        'locked all the other doors until we can do a thorough search. What would you like to do first?'
+primary_menu = {
+    1: 'Examine the scene of the crime',
+    2: 'Question the primary suspect',
+    3: 'Examine the other rooms in the manor',
+    4: 'Question the other witnesses'
+}
+scene_menu = {
+    1: 'Examination of the body',
+    2: 'Examination of the fireplace area',
+    3: 'Examination of the desk area'
+}
+body_menu = {
+    1: 'Examination of the gun',
+    2: 'Examination of the contents of the victim\'s pockets'
+}
+fireplace_menu = {
+    1: 'Examination of the snifter glass',
+    2: 'Examination of the whiskey glass',
+    3: 'Examination of the fireplace'
+}
+desk_menu = {
+    1: 'Examination of the locked drawer',
+    2: 'Examination of the bookcase',
+    3: 'Examination of the display case'
+}
+drawer_menu = {
+    1: 'Examination of the will',
+    2: 'Examination of the letter',
+    3: 'Examination of the key',
+    4: 'Examination of the bank book',
+    5: 'Examination of the wallet'
+}
+will_menu = {
+    1: 'Bequest to Cerise Chroma',
+    2: 'Bequest to Kelly Chroma',
+    3: 'Bequest to Azure Chroma',
+    4: 'Bequest to Captain William Lavender',
+    5: 'Bequest to Housekeeper Myrtle Snow',
+    6: 'Bequest to personal physician'
+}
+bookcase_menu = {
+    1: '',
+    2: ''
+}
+display_menu = {
+    1: '',
+    2: ''
+}
+pri_susp_main_menu = {
+    1: f'Detective Chief Inspector {DCI}: “Before I ask any questions, do you have any statement you would like to make?”',
+    2: f'Detective Chief Inspector {DCI}: “Tell me exactly what happened in the study.”',
+    3: f'Detective Chief Inspector {DCI}: “We’re hoping you can help explain a few things. What can you tell us about the day’s events?”'
+}
+pri_susp_sub_menu = {
+    1: 'Detective Chief Inspector {DCI}: “Your shot didn’t kill him. In fact, it seems we was dead for sometime before.”',
+    2: 'Detective Chief Inspector {DCI}: “We only found one gunshot wound. A grazing head shot.”'
+}
+rooms_menu = {
+    1: '',
+    2: ''
+}
+witnesses_menu = {
+    1: '',
+    2: ''
+}
+_menu = {
+    1: '',
+    2: ''
+}
